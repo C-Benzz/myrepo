65de27d2fabd368794538e14886c975668c9cd41
benz <benzchanapa@outlook.com>
2023-01-09 10:44:55 +0700

    update

---
+++ greetings/greetings.go
@@ -0,0 +1,14 @@
+package greetings
+
+import "fmt"
+
+// func main() {
+// 	Hello()
+// }
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Gooo"
+	fmt.Println("Hello,", name, "World!")
+	fmt.Println("DDDD")
+
+	// fmt.Println(name)
+}
